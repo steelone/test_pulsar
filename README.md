c003a735f111e3b19f599402fe5bb4c59055c761
steelone <slavikdnsnk@gmail.com>
2020-10-20 11:34:22 +0300

    before cleaning up the extra code

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # ISSUE: if we have more than 3 active topics it blocks all async operations
 ### NOTES: simple promises are well but http.get is NOT WORKING. 
-### PULSAR blocks async operations as http.get, readFile, etc.
+### PULSAR blocks async operations as http.get, readdir, readFile, etc.
--- app.js
+++ app.js
@@ -4,57 +4,10 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 const Pulsar = require("pulsar-client");
-const swapi = require('swapi-node');
-const { readFileSync } = require('fs');
 
 app.use(express.json());
 // app.use(express.static("express"));
 // default URL for websiteghfgh
-app.use(function(req,res, next){
-    console.log('request starting...');
-
-    var filePath = '.' + req.url;
-    if (filePath == './')
-        filePath = './index.html';
-
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;      
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        case '.wav':
-            contentType = 'audio/wav';
-            break;
-    }
-    console.log('got');
-    try {
-        const content = fs.readFileSync(path.resolve('express', filePath));
-        res.writeHead(200, { 'Content-Type': contentType });
-        res.end(content, 'utf-8');
-    } catch(error) {
-        if(error.code == 'ENOENT'){
-            next();
-        } else {
-            res.writeHead(500);
-            res.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-            res.end(); 
-        }
-    }
-  });
-
 
 const server = http.createServer(app);
 const port = 3000;
@@ -68,7 +21,7 @@ console.debug('Server listening on port ' + port);
 const client = new Pulsar.Client({
     serviceUrl: "pulsar://localhost:6650",
     operationTimeoutSeconds: 30,
-    authentication: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJicm9rZXItYWRtaW4ifQ.apHQuZHv5KiaWrlQSlN4kk_hzG4WD-s7zh74FPgsbF2pVLVgOsFy-JA3kFtTawfYn7xGPFlvKiBkkdvWvque1U4Bx74dsOH67rHU-5We_kdRB75ocrY1jZbN3-EHYzbNMsAuRdpyeEtB5g7Stm-IxgPw0LVlREDeNePVFHqefr6O-NpVaphb6lEynMpSKE25jSdceixcoUeu9_gd3U9Te7JS7jGzn5hiEzDOhNSF-CGg-9a5ENtqmMB7O_5n9a7SqutsBVp8qziTieVLkPIm73x1TKhpN6vSyXMbspW82ySGhmXbS2mSjSCgBUE77aPe7JgISuRmWZ4Ke2QKZhyOiw"
+    authentication: "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJicm9rZXItYWRtaW4ifQ.apHQuZHv5KiaWrlQSlN4kk_hzG4WD-s7zh74FPgsbF2pVLVgOsFy-JA3kFtTawfYn7xGPFlvKiBkkdvWvque1U4Bx74dsOH67rHU-5We_kdRB75ocrY1jZbN3-EHYzbNMsAuRdpyeEtB5g7Stm-IxgPw0LVlREDeNePVFHqefr6O-NpVaphb6lEynMpSKE25jSdceixcoUeu9_gd3U9Te7JS7jGzn5hiEzDOhNSF-CGg-9a5ENtqmMB7O_5n9a7SqutsBVp8qziTieVLkPIm73x1TKhpN6vSyXMbspW82ySGhmXbS2mSjSCgBUE77aPe7JsrtewvbFdsR2CxW2r7G9"
 });
 
 
@@ -84,27 +37,22 @@ const subscribe = async ({
     const consumer = await client.subscribe({
         topic,
         subscription: 'sub1',
-        subscriptionType: 'Shared',
+        subscriptionType,
         ackTimeoutMs: 10000,
     });
-    // try {
         for (;;) {
             try {
                 const msg = await consumer.receive();
                 consumer.acknowledge(msg);
                 onMessage(msg.getData().toString());
             } catch (e) {
-                // if (e.message !== '[Error: Failed to received message TimeOut]') {
-                //     consumer.close();
-                //     onError(e);
-                //     break;
-                // }
+                if (e.message !== '[Error: Failed to received message TimeOut]') {
+                    consumer.close();
+                    onError(e);
+                    break;
+                }
             }
         }
-    // } catch (e) {
-    //     consumer.close();
-    //     onError(e);
-    // }
 };
 
 // Subscribing to pulsar's new comment topic
@@ -123,36 +71,42 @@ function subscribePulsar(topic, handler) {
 function handler(msg){
     console.info('MSG: ', msg);
 }
+
 subscribePulsar("test1", handler);
 subscribePulsar("test2", handler);
 subscribePulsar("test3", handler);
 // subscribePulsar("test4", handler);
 // subscribePulsar("test5", handler);
 
-// setTimeout(()=>{
-//     console.log('LALALA');
-// }, 1000)
-
 let counter = 0
 setInterval(() => {
-    // console.log('MS', fs.readdirSync('./'));
-    const p = new Promise((resolve, reject)=>{
-        if(typeof "someone's name" === "string") {
-            resolve('SUCCESS')
-        } else {
-            reject('Fail')
-        }
-    }).then((data)=>{
-        console.log(data);
-    })
-    var original = Promise.resolve(true);
-    var cast = Promise.resolve(original);
-    cast.then(function(v) {
-    console.log(v); // true
 
-    swapi.get('https://swapi.dev/api/people/?page=2').then((res)=>{
-        console.log(res);
-    })
+    /* CHECKING readFile or readdir */
+    fs.readdir('./', (err, data)=>{
+        console.log(data);
+    });
+
+    /* CHECKING Promises */
+    // const p = new Promise((resolve, reject)=>{
+    //     if(typeof "someone's name" === "string") {
+    //         resolve('SUCCESS')
+    //     } else {
+    //         reject('Fail')
+    //     }
+    // }).then((data)=>{
+    //     console.log(data);
+    // })
+    // var original = Promise.resolve(true);
+    // var cast = Promise.resolve(original);
+    // cast.then(function(v) {
+    //     console.log(v); // true
+    // });
+    
+    /* CHECKING http.get */
+    // const swapi = require('swapi-node');
+    // swapi.get('https://swapi.dev/api/people/?page=2').then((res)=>{
+    //     console.log(res);
+    // })
+
     console.log(`-------------------------${counter++}----------------------------`);
-});
 }, 1000);
